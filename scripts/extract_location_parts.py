a6bfdeac87f96e5ca283117519ae91bd3b51c356
Halász Dávid <david.peter.halasz@gmail.com>
2020-12-03 15:15:57 +0100

    [mod] Modified extract_location_parts.py: Added a county hint set with words implying the type of a location. Also added new elements to abbrs_rus_dict, wchis is a dictionary of locatinos abbreviations.

--- scripts/extract_location_parts.py
+++ scripts/extract_location_parts.py
@@ -40,6 +40,8 @@ pat_num = re.compile(r'\d+')
 
 scale_of_types = ['county', 'district', 'city']  # street]
 
+county_hint_words = {'меде', 'меди', 'медя'}
+
 frequent_countries = [
     # 'Венгр.',
     # 'Венг.',
@@ -80,25 +82,32 @@ abbrs_rus_dict = {
     'обл.': 'county',  # megye
     'окр.': 'county',  # megye
     'о.': 'county',  # megye / kevesebbszer város
+    'ок.': 'county',
     'губ.': 'county',  # megye/tartomány
     'пр.': 'county',  # megye
     'пров.': 'county',  # megye
+    'прав.': 'county',
+    'кр.': 'county',  # megye
+    'к-р': 'county',  # megye
+    'к.': 'county',  # megye
+    'край': 'county',
+    'комитат': 'county',
+    'к-т': 'county',
     'р-н': 'county',  # megye, ha van obl, akkor ált. város/község
     'p-нe': 'county',  # ua., mint egyel feljebb
     'р-он': 'county',
     'р-на': 'county',
-    'кр.': 'county',  # megye
-    'к.': 'county',  # megye
     'у.': 'county',  # megye
     'уезд': 'county',
     'у-д': 'county',
+    'ком.': 'county',  #  уезд után inkáb disctrict, míg a komitat nem.
     'м.': 'district',  # város/község
-    'г.': 'city',  # város
     'с.': 'city',  # város
+    'ст.': 'city',  # város
+    'д.': 'city',  # község vagy házszám (kisebb számban)
+    'г.': 'city',  # város ez lehet disctrict is
     'сл.': 'city',  # város
     'р.': 'city',  # város
-    'д.': 'city',  # község vagy házszám (kisebb számban)
-    'ст.': 'city',  # város
     # 'ул.': 'street'  # utca
 }
 
@@ -127,6 +136,10 @@ def extract_location_parts(string, row_num=None):
 
     is_country_found = False
     for location_info in string.replace(', ', ',').split(','):
+        for county_hint_word in county_hint_words:
+            location_info = location_info.replace(f' {county_hint_word}', county_hint_word)
+            location_info = location_info.replace(f'-{county_hint_word}', county_hint_word)
+
         location_info = location_info.strip()
         abbreviations_per_locaton = set()
 
@@ -162,14 +175,29 @@ def extract_location_parts(string, row_num=None):
         #     continue
 
         current_location_type = ''
-        for poss_abbreviation in current_location_info:
-            poss_abbreviation = poss_abbreviation.lower()
+        is_county_found = False
+        for poss_abbreviation_or_county_hint in current_location_info:
+            for county_hint_word in county_hint_words:
+                if poss_abbreviation_or_county_hint.endswith(county_hint_word):
+                    location_info = poss_abbreviation_or_county_hint[: -len(county_hint_word)]
+                    location_parts['county'] = location_info
+                    is_county_found = True
+                    break
+
+            if is_county_found:
+                break
+
+            poss_abbreviation = poss_abbreviation_or_county_hint.lower()
             if poss_abbreviation in abbrs_rus_dict.keys():
                 abbreviations_per_locaton.add(poss_abbreviation)
                 abbreviations.add(poss_abbreviation)
 
                 location_info = ' '.join([info for info in location_info.split() if info not in abbreviations_per_locaton])
-                current_location_type = abbrs_rus_dict[poss_abbreviation]
+                if len(current_location_type) == '':
+                    current_location_type = abbrs_rus_dict[poss_abbreviation]
+
+        if is_county_found:
+            continue
 
         locations_per_string.append((current_location_type, abbreviations_per_locaton, location_info.strip()))
 
@@ -186,12 +214,13 @@ def extract_location_parts(string, row_num=None):
             if abbreviaton in abb:
                 if len(location_parts[abbrs_rus_dict[abbreviaton]]) == 0:
                     location_parts[abbrs_rus_dict[abbreviaton]] = location_info
+                    del locations_per_string[i]
                 else:
                     scale_of_types_index = scale_of_types.index(abbrs_rus_dict[abbreviaton]) + 1
                     if scale_of_types_index < len(scale_of_types):
                         location_parts[scale_of_types[scale_of_types_index]] = location_info
                         scale_of_types_index += 1
-                del locations_per_string[i]
+                        del locations_per_string[i]
                 break
         if scale_of_types_index == len(scale_of_types):
             break
@@ -231,21 +260,19 @@ def main():
             print(extract_location_parts(string[5]))
             print(extract_location_parts(string[6]))
 
-    # For test
+    # # For test
     # with open('samples/out_locs.txt', 'w', encoding='utf-8') as outpf:
 
         # for line in sys.stdin.readlines():  # with open(sys.stdin)
-        # with open('../samples/random_10000_42.csv', encoding='utf-8') as inputf:
-        #     for line in inputf:
-            # string = line.strip().split('\t')
-            # if len(string) < 7:
-            #     return None
-            # loc_5 = extract_location_parts(string[5], row_num=string[0])
-            # if loc_5 is not None:
-            #     print(loc_5, file=outpf)
-            # loc_6 = extract_location_parts(string[6], row_num=string[0])
-            # if loc_6 is not None:
-            #     print(loc_6, file=outpf)
+        #     string = line.strip().split('\t')
+        #     if len(string) < 7:
+        #         return None
+        #     loc_5 = extract_location_parts(string[5], row_num=string[0])
+        #     if loc_5 is not None:
+        #         print(loc_5, file=outpf)
+        #     loc_6 = extract_location_parts(string[6], row_num=string[0])
+        #     if loc_6 is not None:
+        #         print(loc_6, file=outpf)
 
 
 if __name__ == '__main__':
