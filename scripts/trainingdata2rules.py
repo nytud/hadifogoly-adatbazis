f48daa08986a8dcc6d54f569ccd0994c6e378ed2
Sass Bálint <sass.balint@nytud.hu>
2020-12-20 16:13:25 +0100

    [mod] possibility to merge undecidable chars

--- scripts/trainingdata2rules.py
+++ scripts/trainingdata2rules.py
@@ -1,14 +1,28 @@
 """
-Process training data. Generate rules.
+Process training data to generate rules.
 """
 
-import argparse
 import sys
 
 from collections import defaultdict
 
 
 rules = defaultdict(lambda: defaultdict(int)) # :)
+
+
+# XXX 1-char kódolás -- lehetne mindenhol
+one_chars = {
+#    'gy': 'G',
+}
+
+# XXX [aáo] --> 'A', azaz ezeket nem különítjük el, mert nehéz!
+mergesimplify = {
+    'a': 'A',
+    'á': 'A',
+    'o': 'A',
+#    'g': 'H',
+#    'h': 'H'
+}
 
 
 def main():
@@ -21,14 +35,16 @@ def main():
             continue
         orig, trans = fields[2:4]
 
-        if not ' ' in orig:
-          orig = [ch for ch in orig]
-        else:
-          orig = orig.split(' ')
-        if not ' ' in trans:
-          trans = [ch for ch in trans]
-        else:
-          trans = trans.split(' ')
+        orig = orig.split(' ') if ' ' in orig else [ch for ch in orig]
+        trans = trans.split(' ') if ' ' in trans else [ch for ch in trans]
+
+        # ha szeretnénk 1-kar kódolást -- és miért ne
+        for ch, one_char in one_chars.items():
+            trans = [char.replace(ch, one_char) for char in trans]
+
+        # XXX ha össze akarunk vonni "eldönthetetleneket" egybe
+        for ch, merged in mergesimplify.items():
+            trans = [char.replace(ch, merged) for char in trans]
 
         # XXX ha el akarjuk különíteni a szó elejét és végét
         #orig[0] = '^' + orig[0]
@@ -40,7 +56,15 @@ def main():
             rules[a][b] += 1
 
     # print result with percentages
-    for orig_char, trans_chars in sorted(rules.items()):
+
+    # -- eredeti orosz karakter szerint ábécében
+    #for orig_char, trans_chars in sorted(rules.items()):
+
+    # -- legnagyobb %-os érték szerint = elöl a nehezen eldönthetők
+    for orig_char, trans_chars in sorted(rules.items(),
+            key=lambda rule: max(
+            d / sum(rule[1].values()) for d in rule[1].values())):
+
         summa = sum(trans_chars.values())
         print(orig_char, '->', summa,
             [f'{trans_char} {val/summa:.1%}({val})'
